62dab9be0b0e28645491048bece1232dcb0a01c5
Nathan Rajlich <nathan@tootallnate.net>
2019-02-07 19:24:09 -0800

    Revert breaking API change, always return a string

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,19 @@
 import { format } from 'url';
 import { AddressInfo, Server } from 'net';
 
-export interface Address extends AddressInfo {
-  url: string
-}
-
-export default function listen (server: Server, ...args): Promise<string | Address> {
+export default function listen (server: Server, ...args): Promise<string> {
   return new Promise((resolve, reject) => {
     args.push((err: Error): void => {
       if (err) return reject(err);
-      const addressInfo = server.address();
-      if (typeof addressInfo === 'string') {
-        resolve(addressInfo);
+      const address = server.address();
+      if (typeof address === 'string') {
+        resolve(address);
       } else {
         // TODO: detect protocol type based on `server` shape
         const protocol = 'http:';
-        const { address: hostname, port } = addressInfo;
+        const { address: hostname, port } = address;
         const url = format({ protocol, hostname, port });
-        resolve({ url, ...addressInfo, });
+        resolve(url);
       }
     });
     server.listen(...args);
