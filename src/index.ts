19ab9b17fdb221fcbad7a88b301f90dceadb7ebe
Nathan Rajlich <n@n8.io>
2019-03-28 12:11:57 -0700

    Properly reject `EADDRINUSE` errors (#1)
    * Properly reject `EADDRINUSE` errors
    
    This error is not given to the `listen()` callback, but instead emitted
    as an "error" event on the Server instance, so a listener needs to be
    added for that event and then removed before the Promise is
    resolved/rejected.
    
    Also added `.editorconfig` from https://editorconfig.zeit.sh and added
    `yarn test` command.
    
    * Use standard `assert` module only
    
    * Enable TypeScript `strict` mode

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,36 @@
 import { format } from 'url';
-import { AddressInfo, Server } from 'net';
+import { Server } from 'net';
 
-export default function listen (server: Server, ...args): Promise<string> {
-  return new Promise((resolve, reject) => {
-    args.push((err: Error): void => {
-      if (err) return reject(err);
-      const address = server.address();
-      if (typeof address === 'string') {
-        resolve(address);
-      } else {
-        // TODO: detect protocol type based on `server` shape
-        const protocol = 'http:';
-        const { address: hostname, port } = address;
-        const url = format({ protocol, hostname, port });
-        resolve(url);
-      }
-    });
-    server.listen(...args);
-  });
+export default function listen(server: Server, ...args: any[]): Promise<string> {
+	return new Promise((resolve, reject) => {
+		function cleanup() {
+			server.removeListener('error', onError);
+		}
+		function onError(err: Error) {
+			cleanup();
+			reject(err);
+		}
+		args.push(
+			(err: Error): void => {
+				cleanup();
+				if (err) return reject(err);
+				const address = server.address();
+				if (typeof address === 'string') {
+					resolve(address);
+				} else {
+					// TODO: detect protocol type based on `server` shape
+					const protocol = 'http:';
+					const { address: hostname, port } = address;
+					const url = format({ protocol, hostname, port });
+					resolve(url);
+				}
+			}
+		);
+		server.on('error', onError);
+		try {
+			server.listen(...args);
+		} catch (err) {
+			onError(err);
+		}
+	});
 }
