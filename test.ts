9411dbf272212416693d90e17a3d9933c00446b2
Nathan Rajlich <nathan@tootallnate.net>
2019-03-27 20:40:35 -0700

    Properly reject `EADDRINUSE` errors
    This error is not given to the `listen()` callback, but instead emitted
    as an "error" event on the Server instance, so a listener needs to be
    added for that event and then removed before the Promise is
    resolved/rejected.
    
    Also added `.editorconfig` from https://editorconfig.zeit.sh and added
    `yarn test` command.

--- test.ts
+++ test.ts
@@ -1,18 +1,34 @@
-import net from 'net'
-import test from 'ava'
-import http from 'http'
-import listen from './'
+import test from 'ava';
+import listen from './src';
+import * as assert from 'assert';
+import { createServer, isIPv4, isIPv6 } from 'net';
 
-test('no arguments', async (t) => {
-  const server = net.createServer()
-  const address = await listen(server)
-  const match = /^http\:\/\/(.+)\:\d+$/.exec(address)
-  t.pass(!!match)
+test('No arguments', async t => {
+	const server = createServer();
+	const address = await listen(server);
+	const match = /^http\:\/\/(.+)\:\d+$/.exec(address);
+	t.true(!!match);
 
-  let [ _, host ] = match
-  if (/\[.+\]/.test(host)) {
-    t.pass(net.isIPv6(host.substring(1, host.length - 1)))
-  } else {
-    t.pass(net.isIPv4(host.substring(1, host.length - 1)))
-  }
-})
+	let [_, host] = match;
+	if (/\[.+\]/.test(host)) {
+		t.true(isIPv6(host.substring(1, host.length - 1)));
+	} else {
+		t.true(isIPv4(host.substring(1, host.length - 1)));
+	}
+});
+
+test('EADDRINUSE is thrown', async t => {
+	let err: NodeJS.ErrnoException;
+	const port = 63971;
+	const server = createServer();
+	const address = await listen(server, port);
+
+	const server2 = createServer();
+	try {
+		const address2 = await listen(server2, port);
+	} catch (_err) {
+		err = _err;
+	}
+	assert(err);
+	assert.equal(err.code, 'EADDRINUSE');
+});
