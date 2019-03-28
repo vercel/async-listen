4ca2d950d0be44c09e23818b96ce1936bf4f0cc4
Nathan Rajlich <nathan@tootallnate.net>
2019-03-27 20:49:34 -0700

    Use standard `assert` module only

--- test.ts
+++ test.ts
@@ -3,21 +3,21 @@ import listen from './src';
 import * as assert from 'assert';
 import { createServer, isIPv4, isIPv6 } from 'net';
 
-test('No arguments', async t => {
+test('No arguments', async () => {
 	const server = createServer();
 	const address = await listen(server);
 	const match = /^http\:\/\/(.+)\:\d+$/.exec(address);
-	t.true(!!match);
+	assert(match);
 
 	let [_, host] = match;
 	if (/\[.+\]/.test(host)) {
-		t.true(isIPv6(host.substring(1, host.length - 1)));
+		assert(isIPv6(host.substring(1, host.length - 1)));
 	} else {
-		t.true(isIPv4(host.substring(1, host.length - 1)));
+		assert(isIPv4(host.substring(1, host.length - 1)));
 	}
 });
 
-test('EADDRINUSE is thrown', async t => {
+test('EADDRINUSE is thrown', async () => {
 	let err: NodeJS.ErrnoException;
 	const port = 63971;
 	const server = createServer();
